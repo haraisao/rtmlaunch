08fdc09ad71f1680e5a24245a18447839ecbffbf
Isao Hara <haraisao@gmail.com>
2018-05-21 19:16:10 +0900

    update viewer.py

--- viewer.py
+++ viewer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-# Built-in modules
 import sys
-import logging
 import Tkinter
-import threading
+#import threading
 
 import ScrolledText
 
@@ -14,22 +12,27 @@ class RedirectText(object):
         self.output = text_ctrl
 
     def write(self, string):
-        """"""
+        self.output.configure(state = 'normal')
         self.output.insert(Tkinter.END, string)
- 
+        self.output.configure(state = 'disabled')
 
-class MyViewer(object):
-
+class OutViewer(object):
     def __init__(self):
         self.root = Tkinter.Tk()
         self.frame = Tkinter.Frame(self.root)
         self.frame.pack()
   
-        self.scrtxt = ScrolledText.ScrolledText(self.frame)
+        self.scrtxt = ScrolledText.ScrolledText(self.frame, state='disabled')
         self.scrtxt.configure(font='TkFixedFont')
         self.scrtxt.pack()
 
 
         self.rdtxt = RedirectText(self.scrtxt)
         #sys.stdout = self.rdtxt
+    def setStdout(self):
+        sys.stdout = self.rdtxt
+    
+    def resetStdout(self):
+        sys.stdout = sys.__stdout__
+
 
